5bbaa2967cac8e947905bbf15257054329f31982
alex <a>
2024-03-16 20:23:49 +0400

    Добавьте файлы проекта.

---
+++ 14.4/14.4.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string word;
+    std::cout << "Enter Word/Num: ";
+    std::getline(std::cin, word);
+    std::cout << std::endl;
+
+    std::cout << word << " | lenght: " << word.length() << std::endl;
+    std::cout << std::endl;
+
+    char a;
+    char b;
+    int num = word.length();
+
+    a = word[0];
+    b = word[num - 1];
+
+
+    std::cout << a << " " << b;
+    std::cout << std::endl;
+
+    return 0;
+
+}
